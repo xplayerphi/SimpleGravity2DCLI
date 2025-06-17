5d2c034dbb3d21c597a1422d2fa811f5720d6d70
xplayerphi <icarobritoeda@gmail.com>
2025-06-16 21:14:40 -0400

    fix and feat

--- physics.h
+++ physics.h
@@ -3,27 +3,34 @@
 
 #include <time.h>
 #include <math.h>
-#include <unistd.h> 
 #include <stdlib.h>
 
-#define DELTA_T (1.0 / 60.0)
-#define SCALE 1000
+#ifdef _WIN32
+ #include <windows.h>
+#else
+ #include <unistd.h>
+#endif
+
+#define DELTA_T (1.0/60.0)
 #define MAX_D 4095
-#define MAX_ENTITIES 100
 
 struct Entity {
-    double mass; // kg
-    double s[2]; // position, s[0] == x, s[1] == y
-    double v[2]; // velocity
-    double a[2]; // acceleration
-    double f[2]; // force
-    double r; // radius
+ int id; // id
+ double mass; // kg
+ double s[2]; // position, s[0] == x, s[1] == y
+ double v[2]; // velocity
+ double a[2]; // acceleration
+ double r; // radius
 };
 
-struct Entity *create_body(double mass, double pos[2], double vel[2], double acc[2], double force[2], double radius);
-void check_collisions(); 
-void apply_physics(); 
-void wait_for_next_frame(); 
+struct Entity *create_body(
+int id, double mass, double pos[2], double vel[2], double acc[2], double radius
+);
+ 
+void apply_physics(struct Entity *body); 
+double wait_next_frame(); 
+int u_sleep(int milliseconds);
+char *printf_bodyi(struct Entity *body);
 
 #endif
 
